425774f11bb0962df17e2134fa33ee6f5c4356f0
anchal2107 <anchal2107@yahoo.com>
2021-05-28 18:16:45 +0530

    taggstar project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignUpModal from "./Authentication/SignUpModal";
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpModal />
+    </div>
+  );
+}
+
+export default App;
